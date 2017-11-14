0c6e6a4506f4de64ea7ae195e47f5a60812c700e
peljevic <j.peljevic.17@ucl.ac.uk>
2017-11-14 20:56:00 +0000

    Session04
    Creating prefabs
    Storing and managing prefabs
    Coroutines
    Events and messaging system
    Ray casting

---
+++ Assets/Scripts/School/Session_4.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Null - pointing something that doesn't exist
+//Coroutine - - continuos routines - - starting something , waiting something else to happen, than continue - -  conditional f-on - yield - - IEnumerator
+// Kvaternion - ojlerovi uglovi, cetvrta dimenzija smer iliti blend na primer dve rotiras za prav ugao stvaras blend i dobijas cetrspet
+
+public class Session_4 : MonoBehaviour {
+  
+    //Variables
+
+    public GameObject columnPrefab;
+    IEnumerator CreateColumnCoroutine;
+
+
+    // Use this for initialization
+	void Start () {
+
+        //CreateColumnCoroutine = DropColumns();
+           
+        //float x = Random.Range(0, 100);
+        //float x = UnityEngine.Random.Range(1, 10); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        StartCoroutine(CreateColumnCoroutine);
+        Debug.Log(Time.time);
+
+        if(Time.time>5)
+        {
+            StopCoroutine(CreateColumnCoroutine);
+            StopAllCoroutines();
+        }	
+    }
+
+    //Corountnes
+
+    IEnumerator DropColumns()
+    {
+        while (true)
+        {
+            Vector3 columnPosition = new Vector3(Random.Range(-5f, 2.5f), Random.Range(8f, 15f), Random.Range(-7f, 4f));
+            float x = Random.Range(0, 90);
+            float y = Random.Range(0, 90);
+            float z = Random.Range(0, 90);
+            Quaternion columnRotation = new Quaternion(x,y,z,1);
+            GameObject newColumn = Instantiate(columnPrefab, columnPosition, columnRotation);
+            yield return new WaitForSeconds(5);
+        }
+       
+    } 
+}
---
+++ Assets/Scripts/School/Session_4_Raycasting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Destroy(objectInFront.transform.gameObject);
+//mouse raycast
+
+public class Session_4_Raycasting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Store the object that is in front
+        RaycastHit objectInFront;
+        //ray direction
+        Vector3 castedRayDirection = transform.TransformDirection(Vector3.forward);
+        //Raycasting
+        if (Physics.Raycast(transform.position, castedRayDirection,out objectInFront)) 
+        {
+            string objectInFrontName = objectInFront.transform.name;
+            Debug.Log("There is an object in front of me! It's name is" + objectInFrontName);
+        }
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * 0.01f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * 0.01f);
+        }
+	}
+}
